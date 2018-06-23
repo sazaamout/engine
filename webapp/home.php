bf56113aa2e7d1dcd81501cb32fb31e14f7f49e3
saad zaamout <sazaamout@gmail.com>
2018-06-23 22:12:34 +0000

    sample page to connect to database

--- webapp/home.php
+++ webapp/home.php
@@ -1,16 +1,44 @@
 <?php
-  include '/etc/database.php';
-  
-  $mysqli = new mysqli( $db_endpoint, 
-                        $db_username, 
-                        $db_password,  
-                        $db_name
-                      );
 
-  // read all record from table 
-  $result = $mysqli->query("SELECT * FROM users");
+include "/etc/database.php";
 
-  var_dump($results);
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
-   
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+
+try {
+    $conn = new PDO("mysql:host=$db_endpoint;dbname=$db_name", $db_username, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM user"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>
