2080e6a214cfd7e956a681ff6bcb864674500044
saad zaamout <sazaamout@gmail.com>
2018-06-23 23:19:08 +0000

    triggering the autoscaling group via aws cli

--- bin/webapp_init.sh
+++ bin/webapp_init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#
+# this script should be ran once and deleted
+#
+#
 
+# collect data from client
 echo "What is the database username: "
 read db_username
 
@@ -16,7 +21,21 @@ read db_endpoint
 echo "and finally, the s3 bucket name:"
 read s3_destination
 
+echo "and finally, the s3 bucket name:"
+read autoscaling_name
+
+# populat the database with the sample data i created
 aws s3 cp s3://$s3_destination/db/mydbdump.sql ~/  --profile dev
 mysql -h $db_endpoint -u $db_username -p$db_password -D $db_name < ~/mydbdump.sql 
 
 echo "database was populated with data successfuly"
+
+# starting the autoscaling group
+aws autoscaling update-auto-scaling-group \
+    --auto-scaling-group-name $autoscaling_name
+    --min-size 2 \
+    --max-size 5 \
+    --desired-capacity 2
+    --profile dev
+
+echo "your webapp is launched, should be ready in few minutes"
