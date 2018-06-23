f5813a8a99f467be7aa1908ad2ee40ba0afc759d
saad zaamout <sazaamout@gmail.com>
2018-06-23 21:57:43 +0000

    fixing the script

--- bin/webapp_init.sh
+++ bin/webapp_init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-echo "database username"
-read db_user
 
-echo "database password"
+echo "What is the database username: "
+read db_username
+
+echo "what is the database password:"
 read db_password
 
-echo "database name"
+echo "what is the database name:"
 read db_name
 
-echo "database endpoint"
+echo "how about the database endpoint:"
 read db_endpoint
 
-aws s3 cp s3://$s3_destination/db/mydbdump.sql ~/  --profile dev
-mysql -h $db_endpoint -u $db_username -p$db_password -D $db_name > ~/mydbdump.sql 
+echo "and finally, the s3 bucket name:"
+read s3_destination
 
-#
+aws s3 cp s3://$s3_destination/db/mydbdump.sql ~/  --profile dev
+mysql -h $db_endpoint -u $db_username -p$db_password -D $db_name < ~/mydbdump.sql 
+
+echo "database was populated with data successfuly"
